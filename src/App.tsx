c2e46724ba5d81b45746a00f5a37e68c2846f8aa
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2025-10-20 14:18:53 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Banner from './components/Banner';
-import Link from './components/Link';
+import Header from './components/Header';
 
 function App() {
   return (
   <>
+    <Header/>
     <Banner src="./banner.png" alt="Banner" />
-    <Link href='/' target='_blank'>Link</Link>
-  </>
+    </>
   );
 };
 
