ab9a9a7be584fd58c814bccac639ebabcf1e5644
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2025-10-20 15:44:32 -0300

    feat: movie

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,10 +1,7 @@
 import classNames from 'classnames';
 
 import styles from './Tag.module.css';
-
-type Categoria = '2D' | '3D';
-
-type Censura = 'Livre' | '10 anos' | '12 anos' | '14 anos' | '16 anos';
+import type { Categoria, Censura } from '../../types';
 
 interface TagProps {
   value: Categoria | Censura;
