27af1482546767c596795e9c8fd1386a2ed3a197
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2025-11-15 15:59:06 -0300

    feat: newsletter e combo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Banner from './components/Banner';
 import Header from './components/Header';
 import MovieSection from './components/MovieSection';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
     <Header/>
     <Banner src="./banner.png" alt="Banner" />
     <MovieSection/>
+    <Banner src="./combo.png" alt="Combo" />
+    <Newsletter />
     </>
   );
 };
